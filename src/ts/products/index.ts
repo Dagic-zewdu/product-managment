3ae9c138605dea3e4dd34a5121699c9e0fc8743e
Dagi <dagi.zewdu.dz@gmail.com>
2022-10-04 22:01:28 +0300

    Added ts configuration

---
+++ src/ts/products/index.ts
@@ -0,0 +1,3 @@
+import '../../styles/style.css';
+
+console.log('i am ejected on product');
--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,33 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    index: './src/ts/app.ts',
+    products: './src/ts/products/index.ts',
+  },
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html',
-  })],
+    inject: true,
+    chunks: ['index'],
+    filename: 'index.html',
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/products.html',
+    inject: true,
+    chunks: ['products'],
+    filename: 'products.html',
+  }),
+  ],
   module: {
     rules: [
       {
         test: /\.html$/,
         use: ['html-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
@@ -22,5 +40,8 @@ module.exports = {
         },
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
