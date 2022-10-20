c66d33a2781ac7fb7c08f28853f27e3365c75bd6
Dagi <dagi.zewdu.dz@gmail.com>
2022-10-20 23:44:07 +0300

    Added collapsable sidenav tab

---
+++ src/components/shared/collapsable-select/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Collapse } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+type props = {
+  title: string;
+  options?: Array<{ name: string; linkName: string }>;
+};
+function CollapsableSelect({ title, options }: props) {
+  const [show, setShow] = useState<boolean>(false);
+  return (
+    <li className="sidebar-item  has-sub">
+      <div className="sidebar-link" onClick={() => setShow((s: boolean) => !s)}>
+        <i className="bi bi-stack"></i>
+        <span>{title}</span>
+      </div>
+      <ul className={`submenu ${show ? 'active' : ''}`}>
+        {options?.map(({ name, linkName }, index) => (
+          <li className="submenu-item" key={index}>
+            <Link to={`/${linkName}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+}
+
+export default CollapsableSelect;
