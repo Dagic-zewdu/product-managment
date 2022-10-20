849578bddfb479fcd53bf48c29646be8911ff5ed
Dagi <dagi.zewdu.dz@gmail.com>
2022-10-20 23:05:56 +0300

    finished sidebar

--- src/components/layout/AllContainer.tsx
+++ src/components/layout/AllContainer.tsx
@@ -1,4 +1,6 @@
-import React, { Children, useState } from 'react';
+import React, { Children, RefObject, useEffect, useRef, useState } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
+import useWindowSize from '../../hooks/useWindowSize';
 import Navbar from '../navbar';
 import SideNav from '../sidenav';
 
@@ -6,14 +8,30 @@ type container = {
   children: React.ReactNode;
 };
 function AllContainer({ children }: container) {
-  const [toggle, setToggle] = useState<boolean>(false);
+  const [toggle, setToggle] = useState<boolean>(true);
+  const [button, showButton] = useState<boolean>(false);
+  const ref: any = useRef();
+  useOnClickOutside(ref, () => setToggle(false));
+  const { width } = useWindowSize();
+  useEffect(() => {
+    if (width > 1199) {
+      showButton(false);
+      setToggle(true);
+    } else {
+      showButton(true);
+      setToggle(false);
+    }
+  }, [width]);
+  console.log(width);
   return (
     <React.Fragment>
       <SideNav toggle={toggle} />
       <div id="main">
-        <button className="" onClick={(e) => setToggle((t: boolean) => !t)}>
-          <i className="fa-solid fa-bars"></i>
-        </button>
+        {button && (
+          <button className="" ref={ref} onClick={() => setToggle(true)}>
+            <i className="fa-solid fa-bars"></i>
+          </button>
+        )}
         {children}
       </div>
     </React.Fragment>
