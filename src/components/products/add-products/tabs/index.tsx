19fd99b6d280519d8f4492aace2fd9937e58b75f
Dagi <dagi.zewdu.dz@gmail.com>
2022-10-21 10:43:32 +0300

    Added tab in query

--- src/components/products/add-products/tabs/index.tsx
+++ src/components/products/add-products/tabs/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import useQuery from '../../../../hooks/useQuery';
 
 function AddProductsTabs() {
+  const [tab, setTabs] = useState('single');
+  let [searchParams, setSearchParams] = useSearchParams();
+  const handleTabChange = (event: any) => {
+    setTabs(event);
+    setSearchParams({ tab: event });
+  };
+  const { search } = useLocation();
+  const query = search.split('=')[1];
+  useEffect(() => {
+    setTabs(query);
+  }, [query]);
   return (
     <div className="d-flex align-items-center justify-content-center w-100">
-      <Tabs defaultActiveKey="single" id="uncontrolled-tab-example" className="mb-3">
+      <Tabs
+        defaultActiveKey={tab}
+        id="uncontrolled-tab-example"
+        fill
+        onSelect={(e) => handleTabChange(e)}
+        className="mb-3"
+      >
         <Tab eventKey="single" title="Add Single Product"></Tab>
         <Tab eventKey="bundle" title="Add Bundle Product"></Tab>
         <Tab eventKey="quantity" title="Add Quantity to exist product"></Tab>
