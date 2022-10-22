b8032033d382a2eca798bed0d96c93a1d5955d20
Dagi <dagi.zewdu.dz@gmail.com>
2022-10-22 10:39:00 +0300

    Added custom Input

---
+++ src/components/shared/Input/index.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from 'react';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  size: 'sm' | 'lg' | undefined;
+  value: string | number | string[] | undefined;
+}
+function Input({ label, type, ...props }: Props) {
+  return (
+    <FloatingLabel controlId="floatingInput" label={label} className="mb-3">
+      <Form.Control type={type} {...props} />
+    </FloatingLabel>
+  );
+}
+
+export default Input;
