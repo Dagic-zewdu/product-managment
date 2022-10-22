35a3f1b2859ca2788cef3ea077a0d69d86e28480
Dagi <dagi.zewdu.dz@gmail.com>
2022-10-22 23:47:49 +0300

    Added react select with proper options

--- src/components/products/add-products/Form/index.tsx
+++ src/components/products/add-products/Form/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import CustomCreatableSelect from '../../../shared/CreatableSelect';
 import Input from '../../../shared/Input';
 import UploadImage from '../uploadImage';
-
+const options = [
+  { label: 'Tv', value: 1 },
+  { label: 'refrigerator', value: 2 },
+  { label: 'Dstv', value: 3 },
+];
 function AddProductForm() {
-
   return (
     <div className="row">
       <div className="col-lg-6 mb-3">
@@ -14,6 +18,11 @@ function AddProductForm() {
         <Input label="Quantity" type="number" />
         <Input label="Unit Price" min={1} type="number" className="" />
         <Input label="Selling Price" min={1} type="number" />
+        <CustomCreatableSelect
+          placeholder="Select category"
+          options={options}
+          onChange={(value) => console.log(value)}
+        />
       </div>
     </div>
   );
--- src/components/shared/CreatableSelect/index.tsx
+++ src/components/shared/CreatableSelect/index.tsx
@@ -2,36 +2,40 @@ import React, { useState } from 'react';
 import CreatableSelect from 'react-select/creatable';
 import { components, ControlProps, GroupBase } from 'react-select';
 import styles from './index.module.scss';
-
+type option = { value: string | number; label: string; icon?: string };
 type props = {
-  options: Array<{ value: string | number; label: string | number; icon?: string }>;
+  options: Array<option>;
   isClearable?: boolean;
   onChange: (value: string | number) => void;
-  value?: { value: string | number; label: string | number; icon?: string };
+  placeholder: string;
+  value?: option;
 };
 
-const Control = ({ children, ...props }: ControlProps<props['options']>) => {
-  // @ts-ignore
-  const { label, icon } = props.selectProps;
-  console.log(props);
-  return (
-    <components.Control {...props}>
-      {icon ? (
-        <div className={styles.selectInputContainer}>
-          <img src={icon} alt="" className={styles.inputIconImage} />
-          <p>{label}</p>
+const Control = ({ value, label, icon }: option) => (
+  <React.Fragment>
+    {icon ? (
+      <div className={styles.selectInputContainer}>
+        <img src={icon} alt="" className={styles.inputIconImage} />
+        <p className="mb-0">{label}</p>
+      </div>
+    ) : (
+      <div className={styles.selectInputContainer}>
+        <div className={styles.inputSelectIcon}>
+          <i className="fa-regular fa-print"></i>
         </div>
-      ) : (
-        <div className={styles.selectInputContainer}>
-          <div className={styles.inputSelectIcon}>{label.charAt[0]}</div>
-          <p>{label}</p>
-        </div>
-      )}
-    </components.Control>
-  );
-};
+        <p className="mb-0">{label}</p>
+      </div>
+    )}
+  </React.Fragment>
+);
 
-function CustomCreatableSelect({ options, isClearable = true, value, onChange }: props) {
+function CustomCreatableSelect({
+  options,
+  isClearable = true,
+  value,
+  onChange,
+  placeholder,
+}: props) {
   const [Value, setValue] = useState<props['value']>(value ? value : options[0]);
   const handleChange = (value: props['value']) => {
     setValue(value);
@@ -40,9 +44,9 @@ function CustomCreatableSelect({ options, isClearable = true, value, onChange }:
   };
   return (
     <CreatableSelect
-    
       isClearable={isClearable}
-      components={{ Control }}
+      formatOptionLabel={Control}
+      placeholder={placeholder}
       // @ts-ignore
       options={options}
     />
